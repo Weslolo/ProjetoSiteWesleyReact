bbb7e52afb59232c6c6aff14b3c5db35a5601c27
Wesley Hellstrom Ramos <wesleyhellstromramos@gmail.com>
2021-06-04 15:28:17 -0300

    receitas

--- app/src/database.js
+++ app/src/database.js
@@ -15,48 +15,149 @@ export default [
         id: 1,
         titulo: "Pão sem glútem",
         descricao: "Receita boa e cheia de vitaminas para seu café da manhã.",
-        imagem: receita1
+        imagem: receita1,
+        modoDePreparo: ` Bata todos os ingredientes (menos o fermento) no liquidificador. Coloque o fermento e bata mais uma vez apenas para misturar.
+         Despeje a massa em uma forma para pães (ou para bolo inglês - que foi a que usei) untada com azeite e farinha de arroz. Deixe crescer em local abafado até dobrar de tamanho.
+         Coloque para assar em forno médio pré-aquecido (180 graus) por aproximadamente 30 minutos ou até que o pão esteja totalmente assado.`,
+        ingredientes: [
+            "4 ovos",
+            " 1/2 xíc. (chá) de leite de coco diluído (sendo 1/2 xíc. de leite de coco + 1 xíc. de água)",
+            "4 c. (sopa) de azeite de oliva",
+            " 1/2 xíc. (chá) de farinha de arroz",
+            "1/2 xíc. (chá) de polvilho doce",
+            "1/2 xíc. (chá) de fubá",
+            "1 c. (sopa) de açúcar demerara",
+            "1 c. (chá) de sal marinho (ou à gosto)",
+            "1 c. (sopa) de fermento biológico seco para pães",]
     },
     {
         id: 2,
         titulo: "Pastel",
         descricao: "Receita boa e cheia de vitaminas para seu café da manhã.",
-        imagem: receita2
+        imagem: receita2,
+        modoDePreparo: 'Em uma vasilha, misture primeiro a farinha, o amigo, a goma guar e o sal. Adicione os ovos, a cachaça e o azeite. Vá misturando e acrescentando água morna, até encontro o ponto desejado da massa. Cubra a vasilha e deixe descansar por alguns minutos.  Em uma superfície coberta com plástico filme, abra a massa com a ajuda do Rolo. Corte a massa com um copo, recheie da forma que quiser e feche pressionando as bordas com um garfo. Frite em óleo quente, escorra e sirva!',
+        ingredientes: [
+            "200g de farinha de arroz;",
+            "10g de amido de milho;",
+            "2 ovos;",
+            "Água morna;",
+            "2 colheres de azeite;",
+            "1 colher de cachaça;",
+            "1 colher de chá de goma guar;",
+            "½ colher de chá de sal.",
+        ]
     },
     {
         id: 3,
         titulo: "Bolacha",
         descricao: "Receita boa e cheia de vitaminas para seu café da manhã.",
-        imagem: receita3
+        imagem: receita3,
+        modoDePreparo: 'Em uma tigela misture os ingredientes um a um até formar uma massa uniforme. Quando começar a desgrudar da vasilha é porque está no ponto certo. Faça as bolinhas do biscoito com ajuda de uma colher e disponha em uma assadeira de silicone ou untada com manteiga ou óleo. Leve ao forno médio (180°C) pré-aquecido por 15 minutos ou até dourar. Espere amornar e sirva.',
+        ingredientes: [
+            "1 xícara de chá de farinha de arroz;",
+            "1 xícara de chá de fécula de batata;",
+            "4 colheres de sopa de manteiga;",
+            "2 colheres de sopa de leite de coco;",
+            "½ xícara de chá de adoçante culinário em pó;",
+            "1 colher de sopa de linhaça em pó.",
+        ]
+
     },
     {
         id: 4,
         titulo: "Macarrão caseiro",
         descricao: "Receita boa e cheia de vitaminas para seu café da manhã.",
-        imagem: receita4
+        imagem: receita4,
+        modoDePreparo: 'Selecione um local de trabalho – você pode usar uma bancada ou uma tigela funda para manipular os ingredientes.Despeje a farinha no local escolhido.Faça um buraco no meio da farinha e coloque os ovos um a um, o azeite e a água.Cuidadosamente, vá incorporando os ingredientes à massa e depois de tudo misturado trabalhe a massa para torná-la macia e homogênea.Com uma faca, divida a massa em pequenas porções para trabalhar na máquina de macarrão.Depois, basta cozinhar no modelo padrão: ferva a água e adicione um fio de óleo e sal, cozinhe o macarrão por aproximadamente 5 minutos.',
+        ingredientes: [
+            "250 g	Farinha multiuso sem glúten",
+            "3	ovos",
+            "3 col. de sopa de	azeite de oliva",
+            "3 col. de sopa de	água",
+        ]
     },
     {
         id: 5,
         titulo: "Torta",
         descricao: "Receita boa e cheia de vitaminas para seu café da manhã.",
-        imagem: receita5
+        imagem: receita5,
+        modoDePreparo: 'Em uma tigela misture todos os ingredientes secos.Adicione o óleo de coco e mexa com a ponta dos dedos ou com colher até que fique com aparência de areia molhada.Adicione o ovo e misture para que a massa fique homogênea. Forme uma bolinha com a massa – caso necessário acrescente um pouco de água.Forre o fundo e lateral de uma forma de torta com fundo removível de aproximadamente 19cm. Pressione bem a massa.Faça furos nela com um garfo e asse em forno pré-aquecido a 180º por uns 15/20 minutos ou até dourar.modo de preparo de recheio Derreta o chocolate. Em um processador bata o abacate até ficar um creme.Incorpore o abacate no chocolate até que fique homogêneo.Quando a massa da torta estiver assada e fria, despeje o creme.Deixe na geladeira até servir! ',
+        ingredientes: [
+            "MASSA",
+            "Farinha de Coco 1 xícara (chá)",
+            "Farinha de grão-de-bico 1 xícara (chá)",
+            "Polvilho Doce 3 colheres (sopa)",
+            "Óleo de coco 6 colheres (sopa)",
+            "Açúcar de coco 1 colher (sopa)",
+            "Ovo 1 unidade",
+            "Sal 1 pitada",
+            "RECHEIO",
+            "Farinha de Coco 1 xícara (chá) ",
+            "Farinha de grão-de-bico 1 xícara (chá)",
+            "Polvilho Doce 3 colheres (sopa)",
+            " Óleo de coco 6 colheres (sopa)",
+            "Açúcar de coco 1 colher (sopa)",
+            "Ovo 1 unidade",
+            "Sal 1 pitada",
+            "Abacate 1/2 unidade",
+            "Chocolate 70% cacau 110 gramas",
+        ]
     },
     {
         id: 6,
         titulo: "Esfiha aberta",
         descricao: "Receita boa e cheia de vitaminas para seu café da manhã.",
-        imagem: receita6
+        imagem: receita6,
+        modoDePreparo: 'Modo de preparo do Recheio de carne: Em uma panela, aqueça o azeite e refogue a cebola e o alho.Adicione a carne e refogue até dourar.Tempere com o sal, a pimenta e a salsinha. Modo de preparo da massa  Cozinhe a batata doce.Em um recipiente pequeno, misture o açúcar com o fermento e a água. Reserve por 10 minutos para dobrar de volume.Em uma tigela grande, misture os ingredientes e por último acrescente o fermento.Mexa com as mãos até obter uma massa homogênea e que não grude.Deixe descansar por cerca de 30 minutos coberta com um pano de prato seco.Em seguida, retire pequenas porções da massa e modele as esfias, pincele a gema nas bordas.Disponha em uma forma untada levemente com azeite e leve para assar em forno preaquecido (180°C) por cerca de 20 minutos.Retire as esfias e acrescente o recheio.Retorne ao forno por 5 minutos para aquecer.Sirva em seguida.',
+        ingredientes: [
+            "Para a massa",
+            "1 col. de sopa de	açúcar Demerara",
+            "10 g	fermento biológico seco",
+            "1/2 xícara de chá de	água morna",
+            "1 e 1/2 xícara de chá de	Farinha multiuso sem glúten",
+            "1/2 xícara de chá de	batata doce cozida",
+            "1/4 xícara de chá de	azeite de oliva",
+            "gema de ovo para pincelar",
+            "Ingredientes Para o recheio",
+            "2 col. de sopa de	azeite de oliva",
+            "1	cebola picada",
+            "1	dente de alho picado",
+            "2 xícaras de chá de	carne moída",
+            "1 col. de chá de	sal",
+            "1/2 col. de chá de	pimenta do reino recém-moída",
+            "2 col. de sopa de	salsinha picada",              
+        ]
     },
     {
         id: 7,
         titulo: "Pão de queijo",
         descricao: "Receita boa e cheia de vitaminas para seu café da manhã.",
-        imagem: receita7
+        imagem: receita7,
+        modoDePreparo: 'Reserve o parmesão e bata os demais ingredientes no liquidificador. Junte o parmesão e pulse somente para misturar.Com uma concha, espalhe uma porção da massa na chapa da máquina de waffle já quente (ou sanduicheira), fazendo uma fina camada. Feche e deixe cozinhar por cerca de 5 minutos ou até dourar. Sirva em seguida.',
+        ingredientes: [
+            "3 ovos",
+            "2 xícaras (chá) de leite",
+            "1/2 xícara (chá) de óleo",
+            "500 g de Polvilho azedo ou doce",
+            "1 colher (chá) de Sal",
+            "150 g de parmesão ralado",
+        ]
     },
     {
         id: 8,
         titulo: "Pizza",
         descricao: "Receita boa e cheia de vitaminas para seu café da manhã.",
-        imagem: receita8
+        imagem: receita8,
+        modoDePreparo: 'Em um recipiente misture o polvilho doce, a farinha de arroz, o açúcar e o sal. Adicione o fermento e a água morna.Junte os ovos, ligeiramente batidos, e o azeite e misture bem com uma colher.Misture com as mãos para incorporar bem os ingredientes (não é necessário sovar, pois a massa não contém glúten).Abra a massa com as mãos sobre uma assadeira de pizza untada com azeite (35 cm de diâmetro) e deixe descansar por 30 minutos. Leve para pré-assar em forno médio-alto (200 °C), preaquecido, por 10 minutos.Coloque o recheio desejado e retorne ao forno para aquecer.Sirva a seguir.',
+        ingredientes: [
+            "1 e meia xícara (chá) de polvilho doce",
+            "1 e meia xícara (chá) de farinha de arroz",
+            "2 colheres (chá) de açúcar",
+            "1 colher (chá) de sal",
+            "1 envelope de fermento biológico seco",
+            "meia xícara (chá) água morna",
+            "2 ovos",
+            "meia xícara (chá) de azeite",
+        ]
     },
 ]
--- app/src/pages/Ver/Ver.js
+++ app/src/pages/Ver/Ver.js
@@ -2,24 +2,40 @@ import React from 'react';
 
 // import { Container } from './styles';
 import receitas from '../../database';
-
+import './ReceitaVer.css';
 import {
-    Redirect,
-    useParams
-  } from "react-router-dom";
+  Redirect,
+  useParams
+} from "react-router-dom";
 function Ver() {
-    let {id} = useParams();
+  let { id } = useParams();
 
-    let _receita = receitas.filter(x=> x.id == id)[0];
+  let _receita = receitas.filter(x => x.id == id)[0];
 
 
 
 
   return (<div>
-      <img className="imagem__ver" src={_receita.imagem} />
-      <h3>{_receita.titulo}</h3>
+    <div className="receita__container">
+      <div className="receita__imagem__container">
+        <img className="receita__imagem__ver" src={_receita.imagem} />
+      </div>
+      <h1>{_receita.titulo}</h1>
       <p>{_receita.descricao}</p>
-  </div>);
+      <h2>Ingredientes</h2>
+      <ul>
+        {_receita.ingredientes != null ? _receita.ingredientes.map((x,index)=>{
+          return (<li key={index}>{x}</li>)
+        })
+      : <p>Sem Ingredientes</p>
+
+      }
+      </ul>
+      <h2>Modo de Preparo</h2>
+      <p>{_receita.modoDePreparo??"Sem Modo de Preparo"}</p>
+    </div>
+  </div>
+  );
 }
 
 export default Ver;
